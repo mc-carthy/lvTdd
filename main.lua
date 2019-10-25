57fda264b52af03287ee864ffa4ec5ae0d61ac77
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-10-25 17:08:25 +0100

    Add functions to allow stubbing of mouse/keyboard input

--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
+keysPressed = {}
+keysDown = {}
+mousePressed = {}
+mouseDown = {}
+
+
 function love.load()
 
 end
 
 function love.update(dt)
-
+    updateKeyboardMouse()
+    print(keysDown['a'])
 end
 
 function love.draw()
@@ -11,7 +18,29 @@ function love.draw()
 end
 
 function love.keypressed(key)
+    keysPressed[key] = true
+    keysDown[key] = true
     if key == 'escape' then
         love.event.quit()
     end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+    mousePressed[button] = true
+end
+
+function updateKeyboardMouse()
+    for k, v in pairs(keysDown) do
+        if not love.keyboard.isDown(k) then
+            keysDown[k] = false
+        end
+    end
+    -- TODO: Implement the below
+    -- for k, v in pairs(mouseDown) do
+    --     if not love.mouse.isDown(k) then
+    --         mouseDown[k] = false
+    --     end
+    -- end
+    keysPressed = {}
+    mousePressed = {}
 end
