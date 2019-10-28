348eb8875bb4357d2bf5a52ba0819690308b4c0d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-10-28 20:49:24 +0000

    Create paddle

---
+++ src/entities/paddle.lua
@@ -0,0 +1,34 @@
+local Paddle = {}
+
+local speed = 250
+local size = {
+    x = 20,
+    y = 80
+}
+
+function Paddle:init(params)
+    local inst = {}
+
+    inst.x = params.x or 0
+    inst.y = params.y or 0
+
+    inst.update = self.update
+    inst.draw = self.draw
+
+    return inst
+end
+
+function Paddle:update(dt)
+    if Input.keyDown('down') then
+        self.y = self.y + speed * dt
+    end
+    if Input.keyDown('up') then
+        self.y = self.y - speed * dt
+    end
+end
+
+function Paddle:draw()
+    love.graphics.rectangle('fill', self.x, self.y, size.x, size.y)
+end
+
+return Paddle
