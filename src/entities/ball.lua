d840a303ebd460fcebb2c66aad3ed1f12b73054d
Mick McCarthy <mick@mccarthy.tech>
2019-11-14 12:47:04 +0000

    Fill out Ball functions

--- src/entities/ball.lua
+++ src/entities/ball.lua
@@ -1,15 +1,23 @@
 local CONST = require('src.utils.constants')
+local Vector2 = require('src.utils.vector2')
 local Ball = {}
+
+local speed = 100
 
 function Ball:init(params)
     local inst = {}
 
-    inst.x = params.x or CONST.SCREEN.WIDTH / 2
-    inst.y = params.y or CONST.SCREEN.HEIGHT / 2
+    inst.pos = Vector2:init{
+        x = params.x or CONST.SCREEN.WIDTH / 2,
+        y = params.y or CONST.SCREEN.HEIGHT / 2
+    }
+    inst.direction = params.direction or 0
+    inst.speed = speed
 
     inst.update = self.update
     inst.draw = self.draw
 
+    inst.setPos = self.setPos
     inst.getDirection = self.getDirection
     inst.getSpeed = self.getSpeed
     inst.getPos = self.getPos
@@ -19,23 +27,39 @@ function Ball:init(params)
 end
 
 function Ball:update(dt)
+    local deltaPos = Vector2:init{
+        x = math.cos(self.direction) * self.speed * dt,
+        y = math.sin(self.direction) * self.speed * dt
+    }
 
+    self:setPos(self:getPos() + deltaPos)
 end
 
 function Ball:draw()
-    
+    love.graphics.setColor(1, 0, 1)
+    love.graphics.circle('fill', self.pos.x, self.pos.y, 10)
 end
 
 function Ball:getDirection()
-
+    return self.direction
 end
 
 function Ball:getSpeed()
-
+    return self.speed
 end
 
 function Ball:getPos()
+    return self.pos
+end
 
+function Ball:setPos(a, b)
+    -- Assume a is vector, add explicit check here
+    if b == nil then
+        self.pos = a
+    else
+        self.pos.x = a
+        self.pos.y = b
+    end
 end
 
 function Ball:center()
