d5c9c5bca86082cfd8d3e7cc5fe8a38e6600453f
Mick McCarthy <mick@mccarthy.tech>
2019-11-14 12:48:06 +0000

    Change main loop to use update Input module

--- main.lua
+++ main.lua
@@ -1,20 +1,26 @@
 Input = require('src.utils.input')
 Paddle = require('src.entities.paddle')
+Ball = require('src.entities.ball')
 
 function love.load()
     paddle1 = Paddle:init{
         x = 20, y = 0
+    }
+    ball = Ball:init{
+        x = 20, y = 20
     }
 end
 
 function love.update(dt)
     Input.readInput()
     paddle1:update(dt)
-    Input.clearInput()
+    ball:update(dt)
+    Input.clearPressed()
 end
 
 function love.draw()
     paddle1:draw()
+    ball:draw()
 end
 
 function love.keypressed(key)
